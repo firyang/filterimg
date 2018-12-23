ac14f0618c048f91cfea3453c2ad51035cd2f529
firyang <40376126+firyang@users.noreply.github.com>
2018-12-24 07:27:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /**
- * 该函数从web文件中匹配所引用的图片，然后从原路径copy图片到新的路径
+ * 该方法从web文件中匹配所引用的图片，然后从原路径copy图片到新的路径
  * @param {Array} paths  web文件（html、vue、css）路径 , 绝对路径 eg. ['F:\\xampp\\htdocs\\mt\\css\\']
  * @param {String} src   项目图片原路径, 绝对路径 eg. 'F:\\xampp\\htdocs\\mt\\'
  * @param {String} des   copy引用图片存放到该路径, des不能与src相同, 绝对路径 eg. 'F:\\xampp\\htdocs\\mt\\copy\\'
